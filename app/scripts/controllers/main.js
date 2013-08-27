98979c20bb18f8bf5ee910a739b724bd4bfada6a
cat <cat@catmini.cat9.private>
2013-08-27 16:13:41 -0500

    Summary: Initial commit
    The initial commit of catpoo which is an attempt to create a
    working template for angular apps.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('catpooApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
